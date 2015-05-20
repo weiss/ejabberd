bdbe8ea20ba45b0cd6c16ab6786b3740ecd8fff4
root <root@mail.rechenwerk.net>
2015-05-21 00:10:47 +0200

    remove wrong hook

--- src/ejabberd_c2s.erl
+++ src/ejabberd_c2s.erl
@@ -2797,8 +2797,6 @@ handle_resume(StateData, Attrs) ->
 			    send_element(NewState, NewEl)
 		    end,
 	  handle_unacked_stanzas(NewState, SendFun),
-      ejabberd_hooks:run(mgmt_resend_stanzas_hook, StateData#state.server,
-                         [StateData#state.jid]),
 	  send_element(NewState,
 		       #xmlel{name = <<"r">>,
 			      attrs = [{<<"xmlns">>, AttrXmlns}],
